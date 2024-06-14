e3e22acda5811725bdfa669eb3f798dac5ffeadf
DennisKim56 <dennis.kim56@gmail.com>
2024-06-13 23:18:13 -0400

    Add env port to listen

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@bu-planning-app.n09tuz2.mongodb.net/${process.env.DB_NAME}`
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
     console.log("Connected");
   })
   .catch(() => {
